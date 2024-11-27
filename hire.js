13d4f347118b4fbadc8d80848c2330d1fd24cfab
borakcstborak <mdislamshakib218@gmail.com>
2024-11-27 12:02:12 +0600

    first commit

---
+++ hire.js
@@ -0,0 +1,10 @@
+// submit button modal show   
+  // Show the modal
+  function showModal() {
+    document.getElementById("modal").classList.remove("hidden");
+  }
+
+  // Hide the modal
+  function hideModal() {
+    document.getElementById("modal").classList.add("hidden");
+  }
---
+++ landing.js
@@ -0,0 +1,20 @@
+// Show a specific modal by its ID
+function showModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.showModal();
+    }
+  }
+  
+  // Close a specific modal by its ID
+  function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.close();
+    }
+  }
+
+
+
+
+  
